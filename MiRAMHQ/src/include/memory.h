b9bb34f1b6fcf7607df2fd616fac882d9a779c57
AgussC <agussc360@gmail.com>
2021-04-25 09:44:21 -0300

    update mem

--- MiRAMHQ/src/include/memory.h
+++ MiRAMHQ/src/include/memory.h
@@ -1,16 +1,24 @@
-#ifndef _MEM_H_
-#define _MEM_H_
+#ifndef _MEMORY_H_
+#define _MEMORY_H_
 
 #include <stdio.h>
 #include <stdint.h>
+#include <commons/collections/list.h>
 
 void *memory;
 void *virtualMemory;
 
 typedef struct {
-    int pid;
-    char **segmentTable; // Base Adress | Limit
+    uint32_t id;
+    uint32_t nroSegmento;
 } p_info;
+
+t_list *segmentTable;
+
+typedef struct {
+    uint32_t baseAddr;
+    uint32_t limit;
+} segment;
 
 typedef struct {
     uint32_t pid;
@@ -24,5 +32,17 @@ typedef struct {
     uint32_t ypos;
     uint32_t next;
 } tcb;
+
+// Asigna espacio a memoria
+void *memory_init(int size);
+
+// Libera la memoria y los segmentos asociados
+void memory_destroy(void *memory, t_list *segmentTable);
+
+// Guarda un struct en la memoria | type -> PCB | TCB | TASK
+void save_element(int type, void *object);
+
+// Devuele un buffer con los datos del struct SIN deserializar
+void* get_element(void *memory, segment *address);
 
 #endif

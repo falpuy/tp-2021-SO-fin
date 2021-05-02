6dbfa37440d79e9fd2be932448334fc87887a6da
belenbrinso <bbrinso@frba.utn.edu.ar>
2021-05-01 22:09:17 -0300

    1/5 trabajamos en consola con las funciones que recibe

---
+++ Discordiador/src/headers/consola.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<readline/readline.h>
+#include<sys/socket.h>
---
+++ Discordiador/src/sources/planificador.c
@@ -0,0 +1,4 @@
+// lista de estados
+// hilos que pasan de estado
+// nuestra lista de control (PCB y TCB)
+// algoritmos (FIFO Y RR)

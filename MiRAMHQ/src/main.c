0be6f43e4540b718c61de1fbea2c902eaf20c3be
cgbaldracco <cgbaldracco@gmail.com>
2021-05-05 20:32:22 -0300

    mi branch

--- MiRAMHQ/src/main.c
+++ MiRAMHQ/src/main.c
@@ -2,23 +2,28 @@
 
 int main() {
 
+    // Inicializando
+   
     config = config_create(CONFIG_PATH);
     logger = log_create(ARCHIVO_LOG, PROGRAM, 1, LOG_LEVEL_TRACE);
-
+   
     signal(SIGINT, liberar_memoria);
+    //
 
+    //
     if (!_check_config(config, KEYS)) {
-        log_error(logger, "Error en archivo de configuracion..");
+        log_error(logger, "Error en el archivo de configuracion..");
         liberar_memoria(0);
     }
 
-    log_info(logger, "(╯°o°)ᕗ Mi RAM HQ ejecutando correctamente..");
-
-    // Creo el mapa
-    // create_map(logger);
+    log_info(logger, "Mi RAM HQ ejecutando correctamente..");
+    //
 
     // Inicializo la memoria
     memory = memory_init(config_get_string_value(config, "TAMANIO_MEMORIA"));
+
+    // Creo el mapa
+    // create_map(logger);
 
     // Creo el server
     _select(config_get_string_value(config, "PUERTO"), handler, logger);
@@ -28,7 +33,6 @@ int main() {
 
 void liberar_memoria(int num) {
   log_destroy(logger);
-  config_destroy(config);
+  config_destroy(config); 
 
-  exit(EXIT_FAILURE);
-}
+ 
